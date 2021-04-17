8f377c3463673e8eeab33ed3e5e3e426bee9b8b1
Naskete <1776795871@qq.com>
2021-04-17 11:42:09 +0800

    get total

--- internal/dao/hackassess.go
+++ internal/dao/hackassess.go
@@ -2,24 +2,34 @@ package dao
 
 import (
 	"HackAssess/internal/model"
+	"HackAssess/pkg"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 // Mark 评分
 func Mark(c *gin.Context) error {
 	group := c.Param("group")
+	// TODO 优化代码结构
 	switch group {
 	case "director":
 		var score model.Director
 		_ = c.ShouldBind(&score)
 		score.Position = group
+		score.Tag = strconv.Itoa(score.UserId) + "-" + score.Position + "-" + score.GroupID
+		total := pkg.Sum(score.Product, score.Idea, score.Integrity, score.Interactive, score.Planning, score.Promotion, score.Slogan, score.Research)
+		score.Total = total
 		if e := d.DB.Create(&score).Error; e != nil {
 			return e
 		}
+		// TODO research 0
 	case "product":
 		var score model.Product
 		_ = c.ShouldBind(&score)
 		score.Position = group
+		score.Tag = strconv.Itoa(score.UserId) + "-" + score.Position + "-" + score.GroupID
+		total := pkg.Sum(score.Innovation, score.Rationality, score.Conformity, score.Research, score.Requirements, score.Prototype, score.Competing, score.Req, score.Project, score.Plan)
+		score.Total = total
 		if e := d.DB.Create(&score).Error; e != nil {
 			return e
 		}
@@ -27,6 +37,9 @@ func Mark(c *gin.Context) error {
 		var score model.Design
 		_ = c.ShouldBind(&score)
 		score.Position = group
+		score.Tag = strconv.Itoa(score.UserId) + "-" + score.Position + "-" + score.GroupID
+		total := pkg.Sum(score.Design, score.Logo, score.Beauty, score.Uniformity, score.Friendliness, score.Reliability, score.Font, score.Thinking, score.Bonus)
+		score.Total = total
 		if e := d.DB.Create(&score).Error; e != nil {
 			return e
 		}
@@ -34,6 +47,9 @@ func Mark(c *gin.Context) error {
 		var score model.Front
 		_ = c.ShouldBind(&score)
 		score.Position = group
+		score.Tag = strconv.Itoa(score.UserId) + "-" + score.Position + "-" + score.GroupID
+		total := pkg.Sum(score.Function, score.Fit, score.Layout, score.Encapsulation, score.Resources, score.Docking)
+		score.Total = total
 		if e := d.DB.Create(&score).Error; e != nil {
 			return e
 		}
@@ -41,13 +57,19 @@ func Mark(c *gin.Context) error {
 		var score model.Back
 		_ = c.ShouldBind(&score)
 		score.Position = group
+		score.Tag = strconv.Itoa(score.UserId) + "-" + score.Position + "-" + score.GroupID
+		total := pkg.Sum(score.Integrity, score.Property, score.Scalability, score.Cert, score.Configuration, score.Information, score.Code, score.Database, score.Interface, score.Document, score.Coverage, score.Solution)
+		score.Total = total
 		if e := d.DB.Create(&score).Error; e != nil {
 			return e
 		}
 	case "show":
-		var score model.Director
+		var score model.Show
 		_ = c.ShouldBind(&score)
 		score.Position = group
+		score.Tag = strconv.Itoa(score.UserId) + "-" + score.Position + "-" + score.GroupID
+		total := pkg.Sum(score.Performance, score.Project, score.Framework, score.Emphasize, score.Idea, score.Style, score.Colour, score.Animation, score.Writing)
+		score.Total = total
 		if e := d.DB.Create(&score).Error; e != nil {
 			return e
 		}
